c6ed2aacd955d4090efa2caaef16d426c6f05a91
AlekCawka <awd.ty4ka@gmail.com>
2025-12-17 21:51:37 +0200

    Добавил базовын правила к паролю

---
+++ .history/src/App_20251211221419.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import LoginForm from './components/auth/LoginForm';
+import './App.css';
+
+function App() {
+  const [mode, setMode] = useState('login');
+  const isLogin = mode === 'login';
+
+  return (
+    <div className='app-root'>
+      <div className='auth-card'>
+        <h1 className='auth-title'>
+          {isLogin ? 'Вход' : 'Регистрация'}
+        </h1>
+
+        <div className='auth-toggle'>
+          <button
+            type='button'
+            className={`auth-toggle__btn ${isLogin ? 'auth-toggle__btn--active' : ''}`}
+            onClick={() => setMode('login')}
+          >
+            Вход
+          </button>
+
+          <button
+            type='button'
+            className={`auth-toggle__btn ${!isLogin ? 'auth-toggle__btn--active' : ''}`}
+            onClick={() => setMode('register')}
+          >
+            Регистрация
+          </button>
+        </div>
+
+        <LoginForm />
+
+        {/* {isLogin ? (
+          <LoginForm />
+        ) : (
+          <RegisterForm onSwitchToLogin={() => setMode('login')} />
+        )} */}
+      </div>
+    </div>
+  )
+}
+
+export default App;
